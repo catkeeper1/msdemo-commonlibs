6ca516fa078cf0e895ec8f8a5e7779086acbc925
catkeeper1 <catkeeper1@gmail.com>
2017-09-18 12:15:30 +0800

    Provide javadoc for pagination.

--- pagination/src/main/java/org/ckr/msdemo/pagination/PaginationInterceptor.java
+++ pagination/src/main/java/org/ckr/msdemo/pagination/PaginationInterceptor.java
@@ -7,18 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Custom HandlerInterceptor for parsing header and clear context data.
+ * Intercept HTTP request to parse pagination request info and clear context data after HTTP.
+ * @see <a href="https://docs.spring.io/spring/docs/4.3.7.RELEASE/spring-framework-reference/htmlsingle/#mvc-handlermapping-interceptor">
+ *       spring reference for interceptor
+ *       </a>
  */
 public class PaginationInterceptor implements HandlerInterceptor {
 
     /**
-     * This method is used to trigger {@link PaginationContext#parseRestPaginationParameters()} to parse
+     * Call {@link PaginationContext#parseRestPaginationParameters()} to parse
      * the patination request info from HTTP request.
      *
      * @param request  HttpServletRequest
      * @param response HttpServletResponse
      * @param handler  handler
-     * @return boolean
+     * @return Always return true so that the HTTP request will be handled by other intercepter or controllers.
      * @throws Exception Exception
      */
     @Override
@@ -31,14 +34,26 @@ public class PaginationInterceptor implements HandlerInterceptor {
     }
 
     /**
-     * This method is <strong>NOT</strong> used to modify response header for @ResponseBody and ResponseEntity methods.
+     * Do nothing.
+     *
+     * <p>This method is <strong>NOT</strong> used to modify HTTP response header to return pagination response
+     * info.
+     *
+     * <p>Please read
+     * <a href="https://docs.spring.io/spring/docs/4.3.7.RELEASE/spring-framework-reference/htmlsingle/#mvc-handlermapping-interceptor">
+     * the end of this section
+     * </a> to understand about why this method cannot be used to modify HTTP response header to return pagination
+     * response info.
+     *
+     * <p> For the reason above, {@link RestPaginationResponseAdvice} should be used to return pagination response info
+     * to HTTP client.
      *
      * @param request      HttpServletRequest
      * @param response     HttpServletResponse
      * @param handler      handler
      * @param modelAndView modelAndView
      * @throws Exception Exception
-     * @see <a href="https://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html#mvc-handlermapping-interceptor">spring-framework-reference</a>
+     *
      */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
@@ -47,7 +62,7 @@ public class PaginationInterceptor implements HandlerInterceptor {
     }
 
     /**
-     * This method will call {@link PaginationContext#clearContextData()} to make sure the data stored in
+     * Call {@link PaginationContext#clearContextData()} to make sure the data stored in
      * thread local in PaginationContext is cleared before the thread is released back to the pool.
      *
      * @param request  HttpServletRequest
--- pagination/src/main/java/org/ckr/msdemo/pagination/package-info.java
+++ pagination/src/main/java/org/ckr/msdemo/pagination/package-info.java
@@ -1,23 +1,77 @@
 /**
- * This module to is resolve complicated dynamic query
- * with dynamic pagination like page size limitation and sorting.
- * <p><img src="org.ckr.msdemo.pagination.Interceptor.svg" alt="class diagram">
+ * Provide classes to implement HTTP endpoints that support dynamic pagination query.
+ *
+ * <p>Below features are included:
+ * <ul>
+ *     <li> Dynamic search criteria. For example, the search criteria can be changed base on the user input in UI.
+ *     <li> Allow HTTP client to specify the range of records that should be returned.
+ *     <li> Allow HTTP client to specify the sorting criteria. Multiple sorting criteria can be specified.
+ *     <li> Return the no of total records for that query.
+ * </ul>
+ *
+ * <p>To enable those pagination features, please place annotation {@link org.ckr.msdemo.pagination.EnablePagination}
+ * in any java configuration class in an application.
+ *
+ * <p>This package include below key classes:
+ * <ul>
+ *     <li>{@link org.ckr.msdemo.pagination.PaginationInterceptor}: Intercept HTTP request and parse pagination
+ *     request info. Such as, the range of records, sorting by which fields.
+ *     <li>{@link org.ckr.msdemo.pagination.PaginationContext}: It hold the pagination info(such as, sorting by which
+ *     fields, the total number of records for a query) so that this kind of data can be shared by different
+ *     components.
+ *     <li>{@link org.ckr.msdemo.pagination.RestPaginationResponseAdvice}: Adjust HTTP response to include the
+ *     pagination info that should be returned to HTTP client in the header.
+ *     <li>{@link org.ckr.msdemo.pagination.JpaRestPaginationService}: Implement the pagination query with JPA API.
+ * </ul>
+ *
+ * Below is a sequence diagram illustrate how all those key classes work together:
+ * <p><img src="paginationSequence.svg" alt="sequence diagram">
  * <!--
- * @startuml org.ckr.msdemo.pagination.Interceptor.svg
- * autonumber
- * "Browser" -> "PaginationInterceptor": Send request
- * participant  RestPaginationResponseAdvice
- * "PaginationInterceptor" -> "PaginationContext": Parse request and save to Threadlocal
- * "Controller" -> "JpaRestPaginationService" : Controller handle request with JpaRestPaginationService
- * "JpaRestPaginationService" -> "PaginationContext": Get request info from Threadlocal
- * "JpaRestPaginationService" -> "PaginationContext": execute query with pagination
- * "JpaRestPaginationService" -> "PaginationContext": execute query with total record count
- * "JpaRestPaginationService" -> "PaginationContext": Save pagination and total record count to Threadlocal
- * "Controller" <- "JpaRestPaginationService" : Return result list
- * "PaginationContext" <- "RestPaginationResponseAdvice": Save custom header to response header
- * "Browser" <- "Controller": Return result list with custom header
- * "PaginationInterceptor" -> "PaginationContext": Clear context data in the current thread
- * @enduml .-->
+@startuml paginationSequence.svg
+autonumber
+
+participant  HTTPClient
+participant  PaginationInterceptor
+participant  RestPaginationResponseAdvice
+participant  PaginationContext
+participant  Controller
+participant  Service
+
+participant  JpaRestPaginationService
+"HTTPClient" -> "PaginationInterceptor": Send request
+"PaginationInterceptor" -> "PaginationContext": Parse and store range of records and sorting fields from\
+HTTP request.
+
+
+"PaginationInterceptor" -> "Controller": Pass HTTP request to controller.                                         \
+                                                            \t
+note right
+  Controller is the controller
+  object that provide HTTP
+  endpoint for this
+  pagination query.
+end note
+"Controller" -> "Service": Construct QL and parameters for the query.
+note right
+  Service is the service object that implement
+  the query logic. It should launch the DB
+  transaction.
+end note
+"Service" -> "JpaRestPaginationService" : Pass search criteria to JpaRestPaginationService
+"JpaRestPaginationService" -> "PaginationContext": Retrieve pagination request info.
+"JpaRestPaginationService" -> "JpaRestPaginationService": Retrieve query content from DB.
+"JpaRestPaginationService" -> "JpaRestPaginationService": Retrieve  and total number of \
+records for this query from DB.
+"JpaRestPaginationService" -> "PaginationContext": Save the total number of records.
+"Service" <- "JpaRestPaginationService" : Return result list
+"Controller" <- "Service" : Return result. Commit DB transaction.
+"Controller" -> "RestPaginationResponseAdvice" : Pass HTTP response to it
+"PaginationContext" <- "RestPaginationResponseAdvice": Save total number of records in HTTP response header.
+"RestPaginationResponseAdvice" -> "PaginationInterceptor": Pass HTTP response to it
+"PaginationInterceptor" -> "PaginationContext": Clear context data(pagination info) to release memory.
+"HTTPClient" <- "PaginationInterceptor": Return result list with pagination info.
+@enduml .
+-->
  */
 
 package org.ckr.msdemo.pagination;
