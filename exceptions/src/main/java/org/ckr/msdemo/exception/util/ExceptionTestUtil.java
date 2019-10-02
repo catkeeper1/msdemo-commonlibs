5df5f84a2163743cce8c4e846eeecf86342921e1
catkeeper1 <catkeeper1@gmail.com>
2019-10-02 21:37:06 +0800

    fix pmd issue.

--- exceptions/src/main/java/org/ckr/msdemo/exception/util/ExceptionTestUtil.java
+++ exceptions/src/main/java/org/ckr/msdemo/exception/util/ExceptionTestUtil.java
@@ -1,6 +1,10 @@
 package org.ckr.msdemo.exception.util;
 
 import org.ckr.msdemo.exception.ApplicationException;
+import org.ckr.msdemo.exception.valueobject.ErrorResponse;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
 
 
 /**
@@ -11,22 +15,26 @@ public class ExceptionTestUtil {
     /**
      * Check whether an {@link ApplicationException} include a error message.
      *
+     * @param messageSource  The message source to retrieve message shown to end user.
      * @param appExp  The application exception
      * @param expectedMsgCode    The message code
      * @param expectedMessage    The message
      * @return   Return true if the exception include the error message. Otherwise return false.
      */
-    public static boolean checkErrorMsg(ApplicationException appExp,
+    public static boolean checkErrorMsg(MessageSource messageSource,
+                                        ApplicationException appExp,
                                         String expectedMsgCode,
                                         String expectedMessage) {
 
+        ErrorResponse errorResponse = new ErrorResponse();
 
+        RestExceptionHandler.updateMsgForResponse(errorResponse, appExp, messageSource, Locale.ENGLISH);
 
-        if (appExp.getMessageList() == null) {
+        if (errorResponse.getMessageList() == null) {
             return false;
         }
 
-        for (ApplicationException.ExceptionMessage errorMessage : appExp.getMessageList()) {
+        for (ErrorResponse.ErrorMessage errorMessage : errorResponse.getMessageList()) {
 
             if (expectedMsgCode.equals(errorMessage.getMessageCode())
                 && expectedMessage.equals(errorMessage.getMessage())) {
